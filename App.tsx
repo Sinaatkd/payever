033905c99cb21af2740e1533389b2bd377ac5a90
Sina Amini <general.sin.amini.20@gmail.com>
2025-02-12 18:21:11 +0330

    The initial react native code schematic has been written.

--- App.tsx
+++ App.tsx
@@ -1,118 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useState } from 'react';
+import { SafeAreaView, NativeModule, NativeModules, Button, Alert, ToastAndroid, TextInput, Text } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface PaymentModuleType {
+    initializeSDK: (apiKey: string) => Promise<string>;
+    processPayment: (amount: number) => Promise<boolean>;
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const { MockPaymentModule } = NativeModules as { MockPaymentModule: PaymentModuleType };
+const App = () => {
+    const [APIKey, setAPIKey] = useState<string>('');
+    const [amount, setAmount] = useState<number>(0);
+    const [paymentResult, setPaymentResult] = useState<string>('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    const handleInitialSDKPress = async () => {
+        try {
+            await MockPaymentModule.initializeSDK(APIKey);
+            ToastAndroid.show("api key initialized", ToastAndroid.LONG);
+        } catch (error) { // if api key input is empty we go here
+            Alert.alert('error', error.message);
+        }
+    }
+
+    const handleAPIKeyChange = (value: string) => {
+        setAPIKey(value);
+    }
+
+    const handleAmountChange = (value: string) => {
+        setAmount(Number.parseInt(value));
+    }
+
+    const handlePaymentProcessPress = async () => {
+        try {
+            const result = await MockPaymentModule.processPayment(amount); // return: false or true
+            setPaymentResult(result ? 'Payment Successful' : 'Payment Failed');
+        } catch (error) { // if api key not initialized we go here
+            Alert.alert('error', error.message);
+        }
+    }
+
+    return (
+        <SafeAreaView>
+            <Text>{paymentResult}</Text>
+            <Button onPress={handleInitialSDKPress} title='SDK' />
+            <Button onPress={handlePaymentProcessPress} title='process' />
+            <TextInput placeholder='api key' onChangeText={handleAPIKeyChange} value={APIKey} />
+            <TextInput placeholder='amount' onChangeText={handleAmountChange} keyboardType={'numeric'} value={amount.toString()} />
+        </SafeAreaView>
+    );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
